a7929f5aba067d13204f2515e36fa6c40de9434b
yang ran <yangran1210@gmail.com>
2016-04-02 13:00:02 -0400

    no message

--- A3_ASR/code/gmmClassify.m
+++ A3_ASR/code/gmmClassify.m
@@ -1,7 +1,9 @@
 trainPath = '/u/cs401/speechdata/Training';
 % trainPath = '/h/u8/g5/00/g5ran/Speech_Recognition_401/speechdata/Training';
-gmms = gmmTrain(trainPath, 100, 0.01, 10);
+% gmms = gmmTrain(trainPath, 100, 0.01, 10);
 save( ['gmms.mat'], 'gmms', '-mat');
+gmms = load('gmms.mat', '-mat');
+gmms = gmms.gmms;
 
 testPath = '/u/cs401/speechdata/Testing';
 mfccs = dir([testPath, filesep, '*.mfcc']);
