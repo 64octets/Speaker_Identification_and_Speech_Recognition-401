99d909acf50e632648c08afae25a6a4fc7e17dd3
yang ran <yangran1210@gmail.com>
2016-04-02 19:53:55 -0400

    no message

--- A3_ASR/code/ibmSpeech.m
+++ A3_ASR/code/ibmSpeech.m
@@ -27,14 +27,12 @@ outputName = 'syn.txt';
 outFile = fopen(outputName, 'w');
 txtFiles = dir([dir_test, filesep, 'unkn_*.txt']);
 for i = 1:length(txtFiles)
-    disp(['file name: ', dir_test, filesep, 'unkn_', num2str(i), '.txt']);
     txtFile = textread([dir_test, filesep, 'unkn_', num2str(i), '.txt'], '%s');
     text = '';
     for j = 3:length(txtFile)
         text = [text, txtFile{j}, ' '];
     end
     text = strtrim(text);
-    disp(text);
 
     likFile = ['unkn_', num2str(i), '.lik'];
     likFile = textread(likFile, '%s');
@@ -43,8 +41,8 @@ for i = 1:length(txtFiles)
         voice = 'en-US_MichaelVoice';
     end
     tempFlac = ['syn_unkn_', num2str(i), '.flac'];
-    unix(['env LD_LIBRARY_PATH='''' curl -u f68e017a-2b5e-47ce-8b4d-8d108e75cbe8:S5QhBuRF3quy -X POST --header "Content-Type: application/json" --header "Accept: audio/flac" --data "{''text'':'',' text, '}" "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?voice=', voice, '" > ', tempFlac]);
-    disp(['env LD_LIBRARY_PATH='''' curl -u da15a506-7899-46cf-9929-73f9f566b29d:GkNQhyD4g03H -X POST --header "Content-Type: audio/flac" --header "Transfer-Encoding: chunked" --data-binary @', tempFlac, ' "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true"']);
+    unix(['env LD_LIBRARY_PATH='''' curl -u f68e017a-2b5e-47ce-8b4d-8d108e75cbe8:S5QhBuRF3quy -X POST --header "Content-Type: application/json" --header "Accept: audio/flac" --data "{\\"text\\":\\",' text, '\\"}" "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?voice=', voice, '" > ', tempFlac]);
+    % disp(['env LD_LIBRARY_PATH='''' curl -u da15a506-7899-46cf-9929-73f9f566b29d:GkNQhyD4g03H -X POST --header "Content-Type: audio/flac" --header "Transfer-Encoding: chunked" --data-binary @', tempFlac, ' "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true"']);
     [status, result] = unix(['env LD_LIBRARY_PATH='''' curl -u da15a506-7899-46cf-9929-73f9f566b29d:GkNQhyD4g03H -X POST --header "Content-Type: audio/flac" --header "Transfer-Encoding: chunked" --data-binary @', tempFlac, ' "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true"']);
     disp(result);
     fprintf(outFile, [result, '\n']);
