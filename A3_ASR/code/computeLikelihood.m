f054373c2c2ef9b50c4b02a065f7b534326b1526
yang ran <yangran1210@gmail.com>
2016-04-05 23:32:14 -0400

    no message

--- A3_ASR/code/computeLikelihood.m
+++ A3_ASR/code/computeLikelihood.m
@@ -5,18 +5,8 @@ function [p, L] = computeLikelihood(X, gmm, D, M)
     for t = 1:T
         weightedSum = 0;
         for m = 1:M
-            % temp1 = 0;
-            % temp2 = 1;
-            % for d = 1:D
-            %     temp1 = temp1 + (((X(t, d) - gmm.means(d, m)) ^ 2) / gmm.cov(d, d, m));
-            %     temp2 = temp2 * gmm.cov(d, d, m);
-            % end
             diff_mean = X(t, :) - transpose(gmm.means(:, m));
             b(t, m) = exp((-1 / 2) * diff_mean * gmm.cov(:, :, m) * transpose(diff_mean)) / ((2 * pi) ^ (D / 2) * sqrt(det(gmm.cov(:, :, m))));
-
-            % b(t, m) = mvnpdf(X(t, :), gmm.means(:, m), gmm.cov(:, :, m));
-            % b(t, m) = exp(temp1 / (-2)) / (((2 * pi) ^ (D / 2)) * sqrt(abs(temp2)));
-            disp(b(t, m));
             weightedSum = weightedSum + gmm.weights(1, m) * b(t, m);
         end
         for m = 1:M
