6408b3ae34db5157da6b2602f0225d537baf44e9
yang ran <yangran1210@gmail.com>
2016-04-06 21:13:52 -0400

    no message

--- A3_ASR/code/ibmSpeech.m
+++ A3_ASR/code/ibmSpeech.m
@@ -6,6 +6,7 @@ flacfiles = dir([dir_test, filesep, '*.flac']);
 for i = 1:length(flacfiles)
     fileName = [dir_test, filesep, 'unkn_', num2str(i), '.flac'];
     [status, result] = unix(['env LD_LIBRARY_PATH='''' curl -u da15a506-7899-46cf-9929-73f9f566b29d:GkNQhyD4g03H -X POST --header "Content-Type: audio/flac" --header "Transfer-Encoding: chunked" --data-binary @', fileName, ' "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true"']);
+    disp(result);
     result = regexp(result, '"transcript": "(?<transcript>.+)"\n', 'names');
     result = strtrim(result.transcript);
 
