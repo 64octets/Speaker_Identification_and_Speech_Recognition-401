39c7abb928cdd30bfd033558eb33c9c27ce9aed1
yang ran <yangran1210@gmail.com>
2016-04-08 17:59:11 -0400

    no message

--- A3_ASR/code/gmmClassify.m
+++ A3_ASR/code/gmmClassify.m
@@ -1,5 +1,5 @@
-trainPath = '/u/cs401/speechdata/Training';
-% trainPath = '/h/u8/g5/00/g5ran/Speaker_Identification_and_Speech_Recognition-401/A3_ASR/data';
+% trainPath = '/u/cs401/speechdata/Training';
+trainPath = '/h/u8/g5/00/g5ran/Speaker_Identification_and_Speech_Recognition-401/A3_ASR/data';
 max_iter = 10;
 M = 2;
 
