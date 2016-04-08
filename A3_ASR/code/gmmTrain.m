9adcd12c00323c753579ec58313b554d34cf9596
yang ran <yangran1210@gmail.com>
2016-04-07 20:15:07 -0400

    no message

--- A3_ASR/code/gmmTrain.m
+++ A3_ASR/code/gmmTrain.m
@@ -35,6 +35,7 @@ function gmms = gmmTrain( dir_train, max_iter, epsilon, M )
         improvement = Inf;
         for i = 1:max_iter
             [p, L] = computeLikelihood(X, gmm, D, M);
+            disp(['L: ', L]);
             if isnan(L) || isinf(L)
                 disp('Exit unexpectedlly!');
                 break;
